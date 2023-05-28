0e3e010c02ef5119cda25045fe61b5785718b62f
Michael .C. Amadi <mgregchi[at]gmail.com>
2023-05-28 17:13:43 +0100

    Remove custom error msg

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,6 +8,5 @@ try {
   const content = fs.readFileSync(filePath, 'utf-8');
   console.log(content);
 } catch (error) {
-  console.error('An error occurred while reading the file:');
   console.error(error);
 }
