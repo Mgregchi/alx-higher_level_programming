56d96165da8f9b1acb3cb035e060b9eee2b36c9c
Michael .C. Amadi <mgregchi[at]gmail.com>
2023-06-16 11:13:38 +0100

    Change var from let to const

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-let fs = require('fs');
-let text1 = fs.readFileSync(process.argv[2], 'utf-8');
-let text2 = fs.readFileSync(process.argv[3], 'utf-8');
+const fs = require('fs');
+const text1 = fs.readFileSync(process.argv[2], 'utf-8');
+const text2 = fs.readFileSync(process.argv[3], 'utf-8');
 fs.writeFileSync(process.argv[4], text1 + text2);
-
